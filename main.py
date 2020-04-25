302cee11be61e69a0f4a771d0e07841f53d49e37
alexanderzou <azou00@stuy.edu>
2020-04-24 23:11:37 -0400

    own pic added

--- main.py
+++ main.py
@@ -9,26 +9,24 @@ import math
 view = [0,
         0,
         1];
-ambient = [0,
-           0,
+ambient = [255,
+           255,
            255]
 light = [[0.5,
           0.75,
           1],
-         [0,
+         [255,
           255,
           255]]
 areflect = [0,
             0,
             0]
 dreflect = [0,
-            0,
+            .5,
             0]
 sreflect = [1,
             1,
             1]
-
-
 
 screen = new_screen()
 zbuffer = new_zbuffer()
@@ -39,5 +37,15 @@ t = new_matrix()
 ident(t)
 csystems = [ t ]
 
-
 parse_file( 'script', edges, polygons, csystems, screen, zbuffer, color, view, ambient, light, areflect, dreflect, sreflect)
+
+screen = new_screen()
+zbuffer = new_zbuffer()
+color = [ 0, 255, 0 ]
+edges = []
+polygons = []
+t = new_matrix()
+ident(t)
+csystems = [ t ]
+
+parse_file( 'mypic', edges, polygons, csystems, screen, zbuffer, color, view, ambient, light, areflect, dreflect, sreflect)
